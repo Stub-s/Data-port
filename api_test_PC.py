dcfd2a09933b9ea521259be0af00cf3c78b9298d
devbutt <devin.si.intha@gmail.com>
2023-07-17 15:16:45 -0400

    include pagination

--- api_test_PC.py
+++ api_test_PC.py
@@ -4,20 +4,30 @@ import pandas as pd
 import numpy as np
 
 query = '''
-    query ($id: Int) {
-        Media (id: $id, type: ANIME) {
-            id
-            title {
-                romaji
-                english
-                native
+    query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+        Page (page: $page, perPage: $perPage) {
+            pageInfo {
+                total
+                currentPage
+                lastPage
+                hasNextPage
+                perPage
+            }
+            media (id: $id, search: $search) {
+                id
+                title {
+                    romaji
+                }
             }
         }
     }
+}
 '''
 
 variables = {
-    'id': 15125
+    'search': 'Boku no Hero Academia',
+    'page': 1,
+    'perPage': 10
 }
 
 url = 'https://graphql.anilist.co'
